428efdabccd6cc1a6dfbae7e0225383d46f46333
Berk Pehlivanoğlu <berkpehlivanoglu@teknasyon.com>
2021-11-15 08:39:23 +0300

    Initial Commit

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by BerkPehlivanoğlu on 15.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
